687fed225b57be7cf0ac7a4732ec77139e76de32
QuizApps <benibara3758@gmail.com>
2023-03-22 22:18:05 +0900

    Initial Commit

---
+++ JLPT N2/JLPT_N2App.swift
@@ -0,0 +1,17 @@
+//
+//  JLPT_N2App.swift
+//  JLPT N2
+//
+//  Created by jaemin park on 2023/03/22.
+//
+
+import SwiftUI
+
+@main
+struct JLPT_N2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
